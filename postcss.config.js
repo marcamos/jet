339f02a2b43f8444ee874d8d48658385bb75f0df
Marc Amos <marc@theout.fit>
2020-08-24 11:53:26 -0400

    Remove unneeded call to Tailwind in postcss.config
    Thanks to @ryanccn for the nudge!

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-const tailwindcss = require('tailwindcss')
 const purgecss = require('@fullhuman/postcss-purgecss')
 const cssnano = require('cssnano')
 
 const plugins = [
-  tailwindcss('tailwind.config.js'),
   purgecss({
     content: ['src/**/*.html']
   }),
