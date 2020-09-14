bdf014e095391b94dec67fb010cbb30c101d2577
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2020-09-14 20:53:18 +0800

    feat: remove Tailwind CLI, use PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+const tailwindcss = require('tailwindcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
 
-const plugins = [autoprefixer(), cssnano()];
+const plugins = [tailwindcss(), autoprefixer(), cssnano()];
 
 module.exports = {
   plugins,
