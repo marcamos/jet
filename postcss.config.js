83eb7c16b76fd5d895956708ca30d0a1a5edb62c
Marc Amos <marc@theout.fit>
2020-09-16 16:10:07 -0400

    Minor prep for new tag

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 const tailwindcss = require('tailwindcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
-
 const plugins = [tailwindcss(), autoprefixer(), cssnano()];
 
 module.exports = {
