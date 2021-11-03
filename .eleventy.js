4bf170a16b56f13ecf59902d41f7b13f6d9ad9c9
Marc Amos <marc@theout.fit>
2021-11-03 15:44:04 -0400

    Formatting, and minor adjustments:
    - Have tailwindcss output right to the public directory
    - As a result, trim the 'clean' script

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-    eleventyConfig.addWatchTarget('./src/css/tailwind.css');
-    eleventyConfig.addPassthroughCopy('./src/css/styles.css');
-    eleventyConfig.addPassthroughCopy('./src/img');
-    return {
-        dir: {
-            input: 'src',
-            output: 'public'
-        }
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget('./src/css/tailwind.css');
+  eleventyConfig.addPassthroughCopy('./src/img');
+  return {
+    dir: {
+      input: 'src',
+      output: 'public'
     }
+  }
 }
