5a60f68ea95930ad49f6650c9df28945a824aa7c
Marc Amos <marc@theout.fit>
2021-11-03 11:58:28 -0400

    I'm bad at git — reapplying changes that I missed

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-const tailwindcss = require('tailwindcss');
-const cssnano = require('cssnano');
-const autoprefixer = require('autoprefixer');
-const plugins = [tailwindcss(), autoprefixer(), cssnano()];
-
-module.exports = {
-  plugins
-};
