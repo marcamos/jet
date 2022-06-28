dd8c5b3d9a44dbf0c718e539d33a68e7831b1377
Geshan Manandhar <geshan@gmail.com>
2022-06-28 23:43:50 +1000

    Builds tailwind after eleventy build is done (#125)
    - [x] Uses eleventy.after event to build tailwind after HTML is ready
    
    Co-authored-by: Geshan Manandhar <geshan@simplywallst.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { execSync } = require("child_process");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+
+  eleventyConfig.on("eleventy.after", async () => {
+    console.log("Building tailwind");
+    console.log(execSync("npm run build:tailwind").toString());
+  });
+
   return {
     htmlTemplateEngine: "njk",
     dir: {
