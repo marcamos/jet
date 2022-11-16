97f0f0ff1c59fe63adf20d5ac07a152589572f28
Marc Amos <marc@theout.fit>
2022-11-16 16:33:21 -0500

    Improve build speed
    Fixes #132

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
-  eleventyConfig.on('eleventy.after', async () => {
-    console.log('Building Tailwind…');
-    console.log(execSync('npm run build:tailwind').toString());
-  });
 
   return {
     htmlTemplateEngine: "njk",
